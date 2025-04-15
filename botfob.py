71d5fd3a901c4f98346934234e64af8b1e6e7726
Asanaliev Temirlan <tonyadam050607@gmail.com>
2025-04-15 13:27:36 +0600

    fix

--- botfob.py
+++ botfob.py
@@ -2,12 +2,16 @@ from telegram.ext import Updater, CommandHandler
 from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerChannel
 import asyncio
+import os
+ 
 
-BOT_TOKEN = '7770419737:AAGDQcoZPz8k0FqZdUQ5hPdk0W1GP78I1Ic'  
-API_ID = '25741913'  
-API_HASH = '51d52fa2d86be95219e48dd61bf59645' 
-PHONE = '+996507555894'  
-YOUR_CHAT_ID = '930678932'  
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+API_ID = os.environ.get('API_ID')
+API_HASH = os.environ.get('API_HASH')
+PHONE = os.environ.get('PHONE')
+YOUR_CHAT_ID = os.environ.get('YOUR_CHAT_ID')
+
 
 CHANNELS = ['@findwork', '@codifynews']  
 
@@ -17,23 +21,23 @@ updater = Updater(BOT_TOKEN, use_context=True)
 dp = updater.dispatcher
 
 def start(update, context):
-    update.message.reply_text('Бот запущен! Отслеживаю вакансии.')
+	update.message.reply_text('Бот запущен! Отслеживаю вакансии.')
 
 dp.add_handler(CommandHandler('start', start))
 
 async def check_channels():
-    await client.start(phone=PHONE)
-    while True:
-        for channel_name in CHANNELS:
-            channel = await client.get_entity(channel_name)
-            async for message in client.iter_messages(channel, limit=1): 
-                if message.text:  
-                    if any(keyword in message.text.lower() for keyword in ['Android', 'Kotlin', 'android']):
-                        updater.bot.send_message(chat_id=YOUR_CHAT_ID, text=f"Новая вакансия в {channel_name}:\n{message.text}")
-        await asyncio.sleep(300)  
+	await client.start(phone=PHONE)
+	while True:
+		for channel_name in CHANNELS:
+			channel = await client.get_entity(channel_name)
+			async for message in client.iter_messages(channel, limit=1): 
+				if message.text:  
+					if any(keyword in message.text.lower() for keyword in ['Android', 'Kotlin', 'android']):
+						updater.bot.send_message(chat_id=YOUR_CHAT_ID, text=f"Новая вакансия в {channel_name}:\n{message.text}")
+		await asyncio.sleep(300)  
 
 if __name__ == '__main__':
-    updater.start_polling()
-    loop = asyncio.get_event_loop()
-    loop.create_task(check_channels())
-    loop.run_forever()
+	updater.start_polling()
+	loop = asyncio.get_event_loop()
+	loop.create_task(check_channels())
+	loop.run_forever()
